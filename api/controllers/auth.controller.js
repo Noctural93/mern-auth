3eff30e1294f66be9e0cf5a631618626e7531287
Noctural93 <gowtham313754@gmail.com>
2024-03-09 02:11:16 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
-// import { errorHandler } from "../util/error.js";
+import { errorHandler } from "../util/error.js";
+import Jwt from "jsonwebtoken";
 
 export const signup = async (request, response, next) => {
     const {username, email, password} = request.body;
@@ -14,3 +15,19 @@ export const signup = async (request, response, next) => {
         // next(errorHandler, "smoething went wrong");
     }
 };
+
+export const signin = async (request, response, next) => {
+    const {email, password} = request.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'worng credentials'));
+        const token = Jwt.sign({id: validUser._id}, process.env.JWT_SECRET); // process.env.jwt_secret is extra security
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+        response.cookie('token', token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
+    } catch (err) {
+        next(err)
+    }
+}
